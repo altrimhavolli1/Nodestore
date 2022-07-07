235118db33c548310f57d46d4dc07a660570539c
altrimhavolli1 <ah40162@ubt-uni.net>
2022-07-07 08:17:36 +0200

    Added orders

--- index.js
+++ index.js
@@ -1,26 +1,74 @@
-const path = require('path')
-const express = require('express');
-const bodyParser = require('body-parser')
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error')
+const errorController = require("./controllers/error");
 
-const app = express()
+const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const orderItem = require("./models/order-item");
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+const app = express();
 
-const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || 'http://localhost:'
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(express.static(path.join(__dirname, 'public')));
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || "http://localhost:";
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
-app.use(errorController.get404)
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const OrderItem = require("./models/order-item");
 
-app.listen(PORT, () => console.log(`Server running on: ${HOST}${PORT}`))
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+Product.belongsTo(User, { constrains: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
+
+sequelize
+  // .sync({force: true})
+  .sync()
+  .then((result) => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Trimi", email: "test@test.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    return user.createCart();
+  })
+  .then((cart) => {
+    app.listen(PORT, () => console.log(`Server running on: ${HOST}${PORT}`));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
